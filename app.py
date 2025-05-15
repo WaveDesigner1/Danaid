4c1b7ca8d4f8a9109eb99106417cf5e66963d286
WaveDesigner1 <118994140+WaveDesigner1@users.noreply.github.com>
2025-05-15 10:01:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def load_user(user_id):
 def create_app():
     app = Flask(__name__)
     CORS(app, supports_credentials=True)
-    
+    os.environ['DATABASE_URL'] = 'postgresql://postgres:MxepqyBo0tfXkJCC@db.twojeID.supabase.co:5432/postgres'
     # Konfiguracja bazy danych
     database_url = os.environ.get('DATABASE_URL')
     if database_url:
