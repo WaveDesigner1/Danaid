0c416e4cc67c95c59457bebed3c6829c50be1a7a
WaveDesigner1 <118994140+WaveDesigner1@users.noreply.github.com>
2025-05-21 13:04:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,5 +16,5 @@ app = create_app()
 app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=False)
