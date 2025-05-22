b75dab1d416901a445891546338970a8e7022141
WaveDesigner1 <118994140+WaveDesigner1@users.noreply.github.com>
2025-05-22 23:13:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -259,15 +259,5 @@ def create_app():
             last_update_key = f'last_online_update_{current_user.id}'
             response.set_cookie(last_update_key, str(int(time.time())), max_age=3600)
         return response
-
-    @app.route('/logout', methods=['POST'])
-    def logout():
-        # Wyczyść dane sesji
-        session.clear()
-        
-        return jsonify({
-            'status': 'success',
-            'message': 'Pomyślnie wylogowano'
-        })
     
     return app
